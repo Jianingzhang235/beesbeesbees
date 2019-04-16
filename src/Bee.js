7b741fec2804bdbd96946ef3267194d1743134a1
Jianing Zhang <jianingzhang082@gmail.com>
2019-04-16 00:42:25 -0700

    passed the testes against Bee class functionality

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,24 @@
 var Bee = function() {
+// 	Create a Bee class(subclass), in pseudoclassical style, with:
+// call the Grub superclass
+ Grub.call(this);
+ // an age property that is set to 5
+ this.age = 5;
+ // a color property that is set to yellow
+ this.color = 'yellow';
+// a food property that is inherited from grub
+//if wo do nothing here, will inherit this.food from superclass.
+// an eat method that is inherited from grub
+// a job property that is set to keep on growing
+this.job = 'keep on growing';
 };
+
+// subclass extends superclass
+//set the prototype
+Bee.prototype = Object.create(Grub.prototype);
+// set the constructor
+Bee.prototype.constructor = Bee;
+
+
+
 
